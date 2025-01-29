a59b3f55cd3940bc04adfa0ca4394977c59ba008
Dihfahsih1 <dihfahsihm@gmail.com>
2025-01-29 23:42:26 +0300

    djangoize the templates

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+def dashboard(request):
+    api_url = "http://127.0.0.1:8000/api/accounts/" 
+    response = requests.get(api_url)
+    accounts = response.json() if response.status_code == 200 else []
+
+    return render(request, "frontend/dashboard.html", {"accounts": accounts})
