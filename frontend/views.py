9657100288425823fffa9afbf401f8da3b0cd0a0
Dihfahsih1 <dihfahsihm@gmail.com>
2025-02-05 12:33:01 +0300

    aggregate weekly incomes on the dashboard

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 import requests
 
+from accounts.models import Account, Income
+from django.utils import timezone
+from datetime import timedelta
+from django.db.models import Sum
+
 def dashboard(request):
+    today = timezone.now().date()
+    start_of_week = today - timedelta(days=today.weekday())
+    total_income = Income.objects.filter(date__gte=start_of_week).aggregate(total=Sum('amount'))
     api_url = "http://127.0.0.1:8000/api/accountstypes/list/"
+    total_accounts = Account.objects.count()
     response = requests.get(api_url)
     
     if response.status_code == 200:
@@ -12,7 +21,7 @@ def dashboard(request):
         accounts = []
         account_count = 0
 
-    return render(request, "dashboard.html", {"accounts": accounts, "account_count": account_count})
+    return render(request, "dashboard.html", {"total_income":total_income,"total_accounts":total_accounts,"accounts": accounts, "account_count": account_count})
 
 def create_account_type_form(request):
     return render(request, "Accounts/create_account_type.html")
