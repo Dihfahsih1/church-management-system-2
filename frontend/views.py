9385d66849baaed4a2ad20c30f64ee30fe57b69a
Dihfahsih1 <dihfahsihm@gmail.com>
2025-02-19 05:38:53 +0300

    authenticate all the front end urls

--- frontend/views.py
+++ frontend/views.py
@@ -9,7 +9,9 @@ from django.db.models import Sum
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
-@permission_classes([IsAuthenticated])
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/auth/') 
 def dashboard(request):
     today = timezone.localdate()   
     start_of_week = today - timedelta(days=today.weekday()) 
@@ -22,27 +24,27 @@ def dashboard(request):
     return render(request, "dashboard.html", {"total_accounts_types":total_accounts_types,"total_expenses":total_expenses,
                                               "total_income":total_income,"total_accounts":total_accounts
                                             })
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def create_account_type_form(request):
     return render(request, "Accounts/create_account_type.html")
 
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def create_account_form(request):
     return render(request, "Accounts/create_account_form.html")
 
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def create_income_category_form(request):
     return render(request, "Categories/create_income_category_form.html")
 
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def create_expenditure_category_form(request):
     return render(request, "Categories/create_expenditure_category_form.html")
 
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def add_expenditure_form(request):
     return render(request, "Expenditure/add_expenditure_form.html")
 
-@permission_classes([IsAuthenticated])
+@login_required(login_url='/auth/') 
 def add_income_form(request):
     return render(request, "Incomes/add_income_form.html")
 
